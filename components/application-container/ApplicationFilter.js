47deb2f80ec939c2727419adfdeac9a58c3448b9
Rakan Douli <68261587+Rairakzak@users.noreply.github.com>
2022-07-09 20:09:22 +0200

    vercel-modified

--- components/application-container/ApplicationFilter.js
+++ components/application-container/ApplicationFilter.js
@@ -139,22 +139,22 @@ const StyledCheckbox = styled.div`
   }
 `;
 
-const StyledOptionField = styled.div`
-  margin: 20px 0;
-  Label {
-    display: block;
-    margin: 20px 0;
-    font-weight: 700;
-    color: ${(props) => props.theme.colors.color3};
-  }
-  select {
-    width: 100%;
-    padding: 10px;
-    border: none;
-    color: ${(props) => props.theme.colors.white};
-    background-color: ${(props) => props.theme.colors.color1};
-  }
-`;
+// const StyledOptionField = styled.div`
+//   margin: 20px 0;
+//   Label {
+//     display: block;
+//     margin: 20px 0;
+//     font-weight: 700;
+//     color: ${(props) => props.theme.colors.color3};
+//   }
+//   select {
+//     width: 100%;
+//     padding: 10px;
+//     border: none;
+//     color: ${(props) => props.theme.colors.white};
+//     background-color: ${(props) => props.theme.colors.color1};
+//   }
+// `;
 
 // //       <div></div>
 // <StyledOptionField>
